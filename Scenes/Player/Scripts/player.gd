fc7cb2158b027b109f1a20eec57020fcc1574857
HeroeLegendario <152408218+HeroeLegendario@users.noreply.github.com>
2026-04-26 11:15:48 -0400

    Update player.gd

--- Scenes/Player/Scripts/player.gd
+++ Scenes/Player/Scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 class_name BasePlayer
 
 @export_category("Movement")
-@export var elevator_initial_index: int = 0
+@export_range(0, 100, 1) var elevator_initial_index: int = 0
 @export var elevator_vector_position: Array[Resource] = []
 @export_category("Powers")
 @export var default_powers: Array[Resource] = []
