9de85d910cb08c79c1fc5b19258bb15c1378ea29
HeroeLegendario <152408218+HeroeLegendario@users.noreply.github.com>
2026-04-26 11:12:47 -0400

    added player script

---
+++ Scenes/Player/Scripts/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+class_name BasePlayer
+
+@export_category("Movement")
+@export var elevator_initial_index: int = 0
+@export var elevator_vector_position: Array[Resource] = []
+@export_category("Powers")
+@export var default_powers: Array[Resource] = []
+@export var obtained_powers: Array[Resource] = []
