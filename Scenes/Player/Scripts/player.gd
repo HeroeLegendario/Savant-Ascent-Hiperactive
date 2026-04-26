cca435d2a617fb1b14f847b9a4cd55cba188fda2
HeroeLegendario <152408218+HeroeLegendario@users.noreply.github.com>
2026-04-26 14:04:09 -0400

    Update player.gd

--- Scenes/Player/Scripts/player.gd
+++ Scenes/Player/Scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 class_name BasePlayer
+const MIN_ELEVATORS: int = 0 # Siempre deberia haber un elevador
+const MAX_ELEVATORS: int = 100
 
 @export_category("Movement")
 @export_range(0, 100, 1) var elevator_initial_index: int = 0
 @export var elevator_vector_position: Array[Resource] = []
+
 @export_category("Powers")
 @export var default_powers: Array[Resource] = []
 @export var obtained_powers: Array[Resource] = []
